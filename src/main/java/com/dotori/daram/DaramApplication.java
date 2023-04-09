019df2c02e7da4806c0fa10aab7ae7d98ff70cbd
daramgya <raichucode@gmail.com>
2023-04-09 22:01:58 +0900

    2023-04-09

---
+++ src/main/java/com/dotori/daram/DaramApplication.java
@@ -0,0 +1,13 @@
+package com.dotori.daram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dotori/daram/DaramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dotori.daram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
